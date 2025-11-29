f5fbd085b804950e5b58608b20dd77a97e93a69a
MaxK789 <maksym.kapitulin@nure.ua>
2025-11-29 23:36:22 +0200

    Configured Vite proxy connection to WebApi (HTTP fix)

--- clients/Aviation.Client.Web/vite.config.js
+++ clients/Aviation.Client.Web/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // поправь порт под свой WebApi (посмотри в Rider, обычно 5001/5242 и т.п.)
-const API_TARGET = 'https://localhost:5001'
+const API_TARGET = 'http://localhost:5185'
 
 export default defineConfig({
   plugins: [vue()],
